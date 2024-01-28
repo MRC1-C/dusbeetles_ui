10465489759757b571c973bb78ce5ec8eb823447
quando <quanchopper1234@gmail.com>
2024-01-28 13:14:55 +0700

    update

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { RootState } from '@/app/store';
-import { StringTable } from '@/app/type/type';
 import { Image } from 'antd';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
@@ -26,10 +25,7 @@ const ProductHome = () => {
                     <div className='font-semibold'>{data.name[language].name}</div>
                     <Image alt='image' className='py-8' width={'100%'} src={data.url} preview={false}></Image>
                     {
-                        // data.description[language].des.split('\n')[0] == "$T" ?
-                        //     <StringTable data={data.description[language].des.slice(3,)} />
-                        //     : <pre className='whitespace-pre-wrap'>{data.description[language].des}</pre>
-                            data.description[language].des.split('\n').map((d:any) => d[0]=='.'?<div className='font-bold'>{d.slice(1,)}</div>:<><div>{d}</div><br /></>)
+                            data.description[language].des.split('\n').map((d:any, index: any) => d[0]=='.'?<div key={index} className='font-bold'>{d.slice(1,)}</div>:<div key={index}><div>{d}</div><br /></div>)
                     }
                 </div>
 
